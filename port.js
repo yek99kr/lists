ba09647449a848c79e9c9e39a900fb5fddc753a1
yek99kr <55264386+yek99kr@users.noreply.github.com>
2021-05-18 13:33:37 -0400

    Add files via upload

---
+++ port.js
@@ -0,0 +1,289 @@
+window.onload = function () {
+  $("#but0").hover(
+    function () {
+      $(".picture").prepend('<img class="pic1" src="groove.png">');
+    },
+    function () {
+      $(".picture").find(".pic1").last().remove();
+    }
+  );
+  $("#but01").hover(
+    function () {
+      $(".picture").prepend('<img class="pic1" src="p3.png">');
+    },
+    function () {
+      $(".picture").find(".pic1").last().remove();
+    }
+  );
+  $("#but1").hover(
+    function () {
+      $(".picture").prepend('<img class="pic1" src="web1.jpg">');
+    },
+    function () {
+      $(".picture").find(".pic1").last().remove();
+    }
+  );
+  $("#but2").hover(
+    function () {
+      $(".picture").prepend('<img class="pic3" src="web3.jpg">');
+    },
+    function () {
+      $(".picture").find(".pic3").last().remove();
+    }
+  );
+  $("#but3").hover(
+    function () {
+      $(".picture").prepend('<img class="pic4" src="web2.jpg">');
+    },
+    function () {
+      $(".picture").find(".pic4").last().remove();
+    }
+  );
+  $("#but4").hover(
+    function () {
+      $(".picture").prepend('<img class="pic2" src="web7.jpg">');
+    },
+    function () {
+      $(".picture").find(".pic2").last().remove();
+    }
+  );
+  $("#but5").hover(
+    function () {
+      $(".picture").prepend('<img class="pic4" src="web5.jpg">');
+    },
+    function () {
+      $(".picture").find(".pic4").last().remove();
+    }
+  );
+  $("#but6").hover(
+    function () {
+      $(".picture").prepend('<img class="pic1" src="web6.jpg">');
+    },
+    function () {
+      $(".picture").find(".pic1").last().remove();
+    }
+  );
+  $("#but24").hover(
+    function () {
+      $(".picture").prepend('<img class="pic2" src="web0.png">');
+    },
+    function () {
+      $(".picture").find(".pic2").last().remove();
+    }
+  );
+
+  $("#but7").hover(
+    function () {
+      $(".picture").prepend('<img class="pic1" src="f1.jpg">');
+    },
+    function () {
+      $(".picture").find(".pic1").last().remove();
+    }
+  );
+  $("#but8").hover(
+    function () {
+      $(".picture").prepend('<img class="pic6" src="calen.jpg">');
+    },
+    function () {
+      $(".picture").find(".pic6").last().remove();
+    }
+  );
+
+  $("#but25").hover(
+    function () {
+      $(".picture").prepend('<img class="pic5" src="gd.jpg">');
+    },
+    function () {
+      $(".picture").find(".pic5").last().remove();
+    }
+  );
+  $("#but9").hover(
+    function () {
+      $(".picture").prepend('<img class="pic1" src="ani2.jpg">');
+    },
+    function () {
+      $(".picture").find(".pic1").last().remove();
+    }
+  );
+  $("#but10").hover(
+    function () {
+      $(".picture").prepend('<img class="pic4" src="ani4.jpg">');
+    },
+    function () {
+      $(".picture").find(".pic4").last().remove();
+    }
+  );
+  $("#but11").hover(
+    function () {
+      $(".picture").prepend('<img class="pic2" src="ani1.jpg">');
+    },
+    function () {
+      $(".picture").find(".pic2").last().remove();
+    }
+  );
+
+  $("#but28").hover(
+    function () {
+      $(".picture").prepend('<img class="pic6" src="ani10.jpg">');
+    },
+    function () {
+      $(".picture").find(".pic6").last().remove();
+    }
+  );
+
+  $("#but12").hover(
+    function () {
+      $(".picture").prepend('<img class="pic6" src="ani5.jpg">');
+    },
+    function () {
+      $(".picture").find(".pic6").last().remove();
+    }
+  );
+  $("#but13").hover(
+    function () {
+      $(".picture").prepend('<img class="pic5" src="ani6.jpg">');
+    },
+    function () {
+      $(".picture").find(".pic5").last().remove();
+    }
+  );
+  $("#but14").hover(
+    function () {
+      $(".picture").prepend('<img class="pic5" src="ani7.jpg">');
+    },
+    function () {
+      $(".picture").find(".pic5").last().remove();
+    }
+  );
+  $("#but15").hover(
+    function () {
+      $(".picture").prepend('<img class="pic6" src="ani8.jpg">');
+    },
+    function () {
+      $(".picture").find(".pic6").last().remove();
+    }
+  );
+  $("#but16").hover(
+    function () {
+      $(".picture").prepend('<img class="pic5" src="ani9.jpg">');
+    },
+    function () {
+      $(".picture").find(".pic5").last().remove();
+    }
+  );
+  $("#but17").hover(
+    function () {
+      $(".picture").prepend('<img class="pic4" src="ani3.jpg">');
+    },
+    function () {
+      $(".picture").find(".pic4").last().remove();
+    }
+  );
+  $("#but18").hover(
+    function () {
+      $(".picture").prepend('<img class="pic1" src="ilust1.JPG">');
+    },
+    function () {
+      $(".picture").find(".pic1").last().remove();
+    }
+  );
+  $("#but19").hover(
+    function () {
+      $(".picture").prepend('<img class="pic3" src="ilust3.JPG">');
+    },
+    function () {
+      $(".picture").find(".pic3").last().remove();
+    }
+  );
+  $("#but20").hover(
+    function () {
+      $(".picture").prepend('<img class="pic2" src="ilust6.JPG">');
+    },
+    function () {
+      $(".picture").find(".pic2").last().remove();
+    }
+  );
+  $("#but21").hover(
+    function () {
+      $(".picture").prepend('<img class="pic4" src="ilust5.JPG">');
+    },
+    function () {
+      $(".picture").find(".pic4").last().remove();
+    }
+  );
+  $("#but22").hover(
+    function () {
+      $(".picture").prepend('<img class="pic2" src="gn1.jpg">');
+    },
+    function () {
+      $(".picture").find(".pic2").last().remove();
+    }
+  );
+  $("#but23").hover(
+    function () {
+      $(".picture").prepend('<img class="pic4" src="ilust4.JPG">');
+    },
+    function () {
+      $(".picture").find(".pic4").last().remove();
+    }
+  );
+  $("#but29").hover(
+    function () {
+      $(".picture").prepend('<img class="pic4" src="illust8.JPG">');
+    },
+    function () {
+      $(".picture").find(".pic4").last().remove();
+    }
+  );
+
+  $("#but26").hover(
+    function () {
+      $(".picture").prepend('<img class="pic2" src="chain1.png">');
+    },
+    function () {
+      $(".picture").find(".pic2").last().remove();
+    }
+  );
+  $("#but27").hover(
+    function () {
+      $(".picture").prepend('<img class="pic6" src="case.png">');
+    },
+    function () {
+      $(".picture").find(".pic6").last().remove();
+    }
+  );
+
+  $(".close").click(function () {
+    $(".modal iframe").each(function () {
+      var el_src = $(this).attr("src");
+      $(this).attr("src", el_src);
+    });
+    $("video").each(function () {
+      $(this).get(0).pause();
+    });
+  });
+
+  var modalBtns = document.querySelectorAll("button");
+
+  modalBtns.forEach(function (btn) {
+    btn.onclick = function () {
+      var modal = btn.getAttribute("data-modal");
+
+      document.getElementById(modal).style.display = "block";
+    };
+  });
+
+  var closeBtns = document.querySelectorAll(".close");
+
+  closeBtns.forEach(function (btn) {
+    btn.onclick = function () {
+      var modal = (btn.closest(".modal").style.display = "none");
+    };
+  });
+
+  window.onclick = function (e) {
+    if (e.target.className === "modal") {
+      e.target, (style.display = "none");
+    }
+  };
+};
